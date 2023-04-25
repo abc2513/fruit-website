9a7326ace2bcabb3183604819e3a9d09f96811d4
“陈帆” <2513151911@qq.com>
2023-04-25 14:26:21 +0800

    4/25 mechine页面图表更新

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,28 +1,99 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { Component } from 'react'
 import NowCount from '../components/NowCount';
+import axios from 'axios';
+import HoursReport from '../components/HoursReport';
+import DaySmallCard from '../components/DaySmallCard'
 
-export default function IndexPage() {
-  const history=useHistory();
-  return (
-    <div className='index-page'>
-      
-      <NowCount></NowCount>
-      <div className="card weather">
+export default class IndexPage extends Component {
+  state = {
+    now_data: { now: {} },
+    now_air: { now: {} },
+    days_report: { daily: [{ fxDate: '' }, { fxDate: '' }] },
+    hours_report: { hourly: [] },
+  }
+  getNowWeather(location = '126.64,45.74') {
+    var url = 'api/v7/weather/now?location=' + location
+    axios.get(url)
+      .then((res) => {
+        //console.log(res)
+        this.setState({ now_data: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  getNowAir(location = '126.64,45.74') {
+    var url = 'api/v7/air/now?location=' + location
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({ now_air: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  getDaysReport(location = '126.64,45.74') {
+    var url = 'api/v7/weather/30d?location=' + location
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({ days_report: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  getHoursReport(location = '126.64,45.74') {
+    var url = 'api/v7/weather/24h?location=' + location
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({ hours_report: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  componentDidMount() {
+    //this.getNowWeather();
+    this.getDaysReport();
+    this.getHoursReport();
+    //this.getNowAir();
+  }
+  render() {
+    const history = this.props.history;
+    return (
+      <div className='index-page'>
+
+        <NowCount></NowCount>
+        {/* <div className="card weather">
         <img className="weather_icon" src="./icon/weather.png"></img>
         <div className="weather_text">今日天气</div>
         <img className="weather_days" src="./icon/projectpromotion.png"></img>
-      </div>
-      <div className="card links-list">
-        <div className="links-list_item" onClick={()=>{history.push('/mechine')}}>
-          <img src="./icon/mymachine.png" className="links-list_item_icon" />
-          <div className="links-list_item_text">我的机器</div>
+      </div> */}
+
+        
+        <div className="weather card">
+
+          <DaySmallCard day_report={this.state.days_report.daily[0]}></DaySmallCard>
+          <DaySmallCard day_report={this.state.days_report.daily[1]}></DaySmallCard>
+
         </div>
-        <div className="links-list_item" onClick={()=>{history.push('/project')}}>
-          <img src="./icon/projectpromotion.png" className="links-list_item_icon" />
-          <div className="links-list_item_text">项目介绍</div>
+        <div className="charts card">
+          <HoursReport hours_report={this.state.hours_report}></HoursReport>
+        </div>
+        <div className="links-list card">
+          <div className="links-list_item card" onClick={() => { history.push('/mechine') }}>
+            <img src="./icon/mymachine.png" className="links-list_item_icon" />
+            <div className="links-list_item_text">我的机器</div>
+          </div>
+          <div className="links-list_item card" onClick={() => { history.push('/project') }}>
+            <img src="./icon/projectpromotion.png" className="links-list_item_icon" />
+            <div className="links-list_item_text">项目介绍</div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
--- src/pages/MechinePage.jsx
+++ src/pages/MechinePage.jsx
@@ -1,63 +1,124 @@
-import React from 'react'
+import React, { Component } from 'react'
 import NowCount from '../components/NowCount';
+import HoursReport from '../components/HoursReport2';
+import axios from 'axios';
 
-export default function MechinePage() {
-  return (
-    <div className='mechine-page'>
-      <NowCount></NowCount>
-      <div className="recent-fruit card">
-        <div className="recent-fruit_heading">近期投入水果</div>
-        <div className="recent-fruit_icons">
-          <img className="recent-fruit_icon" src='./icon/banana.png'></img>
-          <img className="recent-fruit_icon" src='./icon/fruit.png'></img>
-          <img className="recent-fruit_icon" src='./icon/orange.png'></img>
+export default class MechinePage extends Component {
+  state={
+    now_data: { now: {} },
+    now_air: { now: {} },
+    days_report: { daily: [{ fxDate: '' }, { fxDate: '' }] },
+    hours_report: {hourly: []},
+  }
+  getNowWeather(location = '126.64,45.74') {
+    var url = 'api/v7/weather/now?location=' + location
+    axios.get(url)
+      .then((res) => {
+        //console.log(res)
+        this.setState({ now_data: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  getNowAir(location = '126.64,45.74') {
+    var url = 'api/v7/air/now?location=' + location
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({ now_air: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  getDaysReport(location = '126.64,45.74') {
+    var url = 'api/v7/weather/30d?location=' + location
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({ days_report: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  getHoursReport(location = '126.64,45.74') {
+    var url = 'api/v7/weather/24h?location=' + location
+    axios.get(url)
+      .then((res) => {
+        console.log(res)
+        this.setState({ hours_report: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  componentDidMount() {
+    //this.getNowWeather();
+    //this.getDaysReport();
+    //this.getHoursReport();
+    //this.getNowAir();
+  }
+  render() {
+    return (
+      <div className='mechine-page'>
+        <NowCount ></NowCount>
+        <div className="recent-fruit card">
+          <div className="recent-fruit_heading">近期投入水果</div>
+          <div className="recent-fruit_icons">
+            <img className="recent-fruit_icon" src='./icon/banana.png'></img>
+            <img className="recent-fruit_icon" src='./icon/fruit.png'></img>
+            <img className="recent-fruit_icon" src='./icon/orange.png'></img>
+          </div>
+          <div className="recent-fruit_info">产物有机酸较多<br />适合观赏花卉</div>
         </div>
-        <div className="recent-fruit_info">产物有机酸较多<br />适合观赏花卉</div>
-      </div>
-      <div className="weather card">
-        <div className="weather_t s-card">
-          <img src="./icon/temperature.png" />
-          <div className="weather_text">26<span className='weather_text_foot'>°C</span></div>
-        </div>
-        <div className="weather_h s-card">
-          <img src="./icon/humidity.png" />
-          <div className="weather_text">63<span className='weather_text_foot'>%</span></div>
-        </div>
-      </div>
-      <div className="charts card">
-        <img className="data-item_icon" src="./icon/projectpromotion.png"></img>
-        <img className="data-item_icon" src="./icon/projectpromotion.png"></img>
-      </div>
-      <div className="data card">
-        <div className="data-item s-card">
-          <img className="data-item_icon" src="./icon/shovel.png"></img>
-          <div className="data-item_content">
-            <div className="data-item_heading">距上次加肥</div>
-            <div className="data-item_data">15<span className='data-foot'>天</span></div>
+        <div className="weather card">
+          <div className="weather_t s-card">
+            <img src="./icon/temperature.png" />
+            <div className="weather_text">18<span className='weather_text_foot'>°C</span></div>
+            {/* {this.state.now_data.now.temp} */}
+          </div>
+          <div className="weather_h s-card">
+            <img src="./icon/humidity.png" />
+            <div className="weather_text">46<span className='weather_text_foot'>%</span></div>
+            {/* {this.state.now_data.now.humidity} */}
           </div>
         </div>
-        <div className="data-item s-card">
-          <img className="data-item_icon" src="./icon/mymachine.png"></img>
-          <div className="data-item_content">
-            <div className="data-item_heading">距上次取肥</div>
-            <div className="data-item_data">15<span className='data-foot'>天</span></div>
-          </div>
+        <div className="charts card">
+           <HoursReport hours_report={this.state.hours_report}></HoursReport>
         </div>
-        <div className="data-item s-card">
-          <img className="data-item_icon" src="./icon/fertilizer.png"></img>
-          <div className="data-item_content">
-            <div className="data-item_heading">现有甲烷</div>
-            <div className="data-item_data">12<span className='data-foot'>g</span></div>
+        <div className="data card">
+          <div className="data-item s-card">
+            <img className="data-item_icon" src="./icon/shovel.png"></img>
+            <div className="data-item_content">
+              <div className="data-item_heading">距上次加肥</div>
+              <div className="data-item_data">15<span className='data-foot'>天</span></div>
+            </div>
           </div>
-        </div>
-        <div className="data-item s-card">
-          <img className="data-item_icon" src="./icon/methane.png"></img>
-          <div className="data-item_content">
-            <div className="data-item_heading">装置外甲烷浓度</div>
-            <div className="data-item_data">正常<span className='data-foot'></span></div>
+          <div className="data-item s-card">
+            <img className="data-item_icon" src="./icon/mymachine.png"></img>
+            <div className="data-item_content">
+              <div className="data-item_heading">距上次取肥</div>
+              <div className="data-item_data">15<span className='data-foot'>天</span></div>
+            </div>
+          </div>
+          <div className="data-item s-card">
+            <img className="data-item_icon" src="./icon/fertilizer.png"></img>
+            <div className="data-item_content">
+              <div className="data-item_heading">现有甲烷</div>
+              <div className="data-item_data">12<span className='data-foot'>g</span></div>
+            </div>
+          </div>
+          <div className="data-item s-card">
+            <img className="data-item_icon" src="./icon/methane.png"></img>
+            <div className="data-item_content">
+              <div className="data-item_heading">装置外甲烷浓度</div>
+              <div className="data-item_data">正常<span className='data-foot'></span></div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
